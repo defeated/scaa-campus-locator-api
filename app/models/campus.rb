1af068527ddd496321a56b9bd5d9696daa2c5339
eddie cianci <defeated2k4@gmail.com>
2016-12-27 21:51:32 -0500

    campus before_validation to add missing "http://" to urls

--- app/models/campus.rb
+++ app/models/campus.rb
@@ -1,2 +1,10 @@
 class Campus < ApplicationRecord
+  before_validation :normalize_url
+
+  private
+
+  def normalize_url
+    return if url.blank? || url.start_with?('http')
+    url.prepend 'http://'
+  end
 end
