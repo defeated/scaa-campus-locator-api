d8cdc25c03437b6c950a94783cac5efe75e72713
eddie cianci <defeated2k4@gmail.com>
2016-12-30 17:04:10 -0500

    coerce latitude/longitude from decimal to float so json is not quoted
      "latitude":"33.42551" (before)
      "latitude":33.42551   (after)

--- app/resources/api/v1/campus_resource.rb
+++ app/resources/api/v1/campus_resource.rb
@@ -4,4 +4,12 @@ class Api::V1::CampusResource < JSONAPI::Resource
   key_type :uuid
 
   attributes :name, :address, :url, :latitude, :longitude
+
+  def latitude
+    @model.latitude.to_f
+  end
+
+  def longitude
+    @model.longitude.to_f
+  end
 end
